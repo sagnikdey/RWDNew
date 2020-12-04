13f5803e9433cc6c7a3cdff44fce8b0ba5ff039b
Sagnik Dey <sagnikdey@SUVADIPs-MacBook-Pro.local>
2020-12-04 08:13:30 +0800

    commit

---
+++ build/precache-manifest.fe6a3f1bfb8e80878d02611a5e41bd7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2230729aea25cef8598ee0d388e97e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd1f13fb1e64d5006b5b",
+    "url": "/static/css/2.144e0331.chunk.css"
+  },
+  {
+    "revision": "db5ae4bc0cbc14b095d2",
+    "url": "/static/css/main.cab0868e.chunk.css"
+  },
+  {
+    "revision": "fd1f13fb1e64d5006b5b",
+    "url": "/static/js/2.69898e7c.chunk.js"
+  },
+  {
+    "revision": "db5ae4bc0cbc14b095d2",
+    "url": "/static/js/main.fed33233.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
